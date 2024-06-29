482313025a4ec098c487a08bd34c702c7772dfd5
Mahdi Zoraghi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2024-06-29 12:30:53 -0400

    chore: add cspell words

--- dist/index.js
+++ dist/index.js
@@ -8,36 +8,33 @@ const dependenciesInformation = [];
 loading.start();
 const dependenciesInfoPremissList = [];
 for (const packageName of Object.keys(allDependencies)) {
-	dependenciesInfoPremissList.push(getPackagePublishDate(packageName));
+    dependenciesInfoPremissList.push(getPackagePublishDate(packageName));
 }
 const infoList = await Promise.all(dependenciesInfoPremissList);
 for (const packageInfo of infoList) {
-	const { packagePublishDate, packageVersion, packageName } = packageInfo;
-	const diffInDays = differenceInDays(new Date(), new Date(packagePublishDate));
-	let area = "red";
-	if (GREEN_AREA(diffInDays)) area = "green";
-	if (YELLOW_AREA(diffInDays)) area = "yellow";
-	if (RED_AREA(diffInDays)) area = "red";
-	dependenciesInformation.push({
-		name: packageName,
-		version: packageVersion,
-		date: packagePublishDate,
-		diffDays: diffInDays,
-		area: area,
-	});
+    const { packagePublishDate, packageVersion, packageName } = packageInfo;
+    const diffInDays = differenceInDays(new Date(), new Date(packagePublishDate));
+    let area = "red";
+    if (GREEN_AREA(diffInDays))
+        area = "green";
+    if (YELLOW_AREA(diffInDays))
+        area = "yellow";
+    if (RED_AREA(diffInDays))
+        area = "red";
+    dependenciesInformation.push({
+        name: packageName,
+        version: packageVersion,
+        date: packagePublishDate,
+        diffDays: diffInDays,
+        area: area,
+    });
 }
 loading.stop();
 for (const dependencyInfo of dependenciesInformation) {
-	const dependencyArea = chalk[dependencyInfo.area];
-	const { name, version, date } = dependencyInfo;
-	const formattedDate = formatDistance(date, new Date(), {
-		addSuffix: true,
-	});
-	console.log(
-		dependencyArea(
-			name.padEnd(30),
-			version.padEnd(30),
-			formattedDate.padEnd(30),
-		),
-	);
+    const dependencyArea = chalk[dependencyInfo.area];
+    const { name, version, date } = dependencyInfo;
+    const formattedDate = formatDistance(date, new Date(), {
+        addSuffix: true,
+    });
+    console.log(dependencyArea(name.padEnd(30), version.padEnd(30), formattedDate.padEnd(30)));
 }
--- dist/utils.js
+++ dist/utils.js
@@ -4,50 +4,44 @@ import util from "node:util";
 import ora from "ora";
 const execPromise = util.promisify(exec);
 const readPackageJson = async () => {
-	const packageJSON = JSON.parse(
-		await fsPromises.readFile("package.json", {
-			encoding: "utf8",
-		}),
-	);
-	return packageJSON;
+    const packageJSON = JSON.parse(await fsPromises.readFile("package.json", {
+        encoding: "utf8",
+    }));
+    return packageJSON;
 };
 export const getAllDependencies = async () => {
-	const packageJSON = await readPackageJson();
-	const allDependencies = {
-		...packageJSON.dependencies,
-		...packageJSON.devDependencies,
-	};
-	return allDependencies;
+    const packageJSON = await readPackageJson();
+    const allDependencies = {
+        ...packageJSON.dependencies,
+        ...packageJSON.devDependencies,
+    };
+    return allDependencies;
 };
 export const getPackageVersion = async (packageName) => {
-	const { stdout: packageVersionStdout } = await execPromise(
-		`npm view ${packageName} version`,
-	);
-	const packageVersion = packageVersionStdout.replace("\n", "");
-	return packageVersion;
+    const { stdout: packageVersionStdout } = await execPromise(`npm view ${packageName} version`);
+    const packageVersion = packageVersionStdout.replace("\n", "");
+    return packageVersion;
 };
 export const getPublishedTimes = async (packageName) => {
-	const { stdout: publishedTimesStdout } = await execPromise(
-		`npm view ${packageName} time --json`,
-	);
-	const publishedTimes = JSON.parse(publishedTimesStdout);
-	return publishedTimes;
+    const { stdout: publishedTimesStdout } = await execPromise(`npm view ${packageName} time --json`);
+    const publishedTimes = JSON.parse(publishedTimesStdout);
+    return publishedTimes;
 };
 export const getPackagePublishDate = async (packageName) => {
-	const packageVersion = await getPackageVersion(packageName);
-	const publishedTimes = await getPublishedTimes(packageName);
-	const packagePublishDate = publishedTimes[packageVersion];
-	return { packagePublishDate, packageVersion, packageName };
+    const packageVersion = await getPackageVersion(packageName);
+    const publishedTimes = await getPublishedTimes(packageName);
+    const packagePublishDate = publishedTimes[packageVersion];
+    return { packagePublishDate, packageVersion, packageName };
 };
 let oraSpinner = null;
 export const loading = {
-	start() {
-		console.time();
-		oraSpinner = ora("Loading...").start();
-	},
-	stop() {
-		oraSpinner?.stop();
-		oraSpinner?.clear();
-		console.timeEnd();
-	},
+    start() {
+        console.time();
+        oraSpinner = ora("Loading...").start();
+    },
+    stop() {
+        oraSpinner?.stop();
+        oraSpinner?.clear();
+        console.timeEnd();
+    },
 };
