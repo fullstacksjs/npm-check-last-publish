6d3801fd380fbe7ce5c5e99921a4a852723b8265
Mahdi Zoraghi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2024-07-13 05:21:38 -0400

    chore: add semantic-release

--- dist/lib/get-package-publish-date.js
+++
@@ -1,8 +0,0 @@
-import { getPackageVersion } from "./get-package-version.js";
-import { getPublishedTimes } from "./get-published-times.js";
-export const getPackagePublishDate = async (packageName) => {
-    const packageVersion = await getPackageVersion(packageName);
-    const publishedTimes = await getPublishedTimes(packageName);
-    const packagePublishDate = publishedTimes[packageVersion];
-    return { packagePublishDate, packageVersion, packageName, publishedTimes };
-};
