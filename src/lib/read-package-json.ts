4dcbe7c414e301946005ecfe71a794788213dd6c
Erfan Goodarzi <63591256+erfan-goodarzi@users.noreply.github.com>
2025-04-27 15:56:41 +0330

    feat: better error handling when package json is missing (#11)

--- src/lib/read-package-json.ts
+++ src/lib/read-package-json.ts
@@ -1,12 +1,28 @@
 import fsPromises from "node:fs/promises";
+import chalk from "chalk";
 
 export const readPackageJson = async () => {
+  let packageContent: string;
+
+  try {
+    packageContent = await fsPromises.readFile("package.json", {
+      encoding: "utf8",
+    });
+  } catch (error) {
+    console.log(
+      chalk.redBright(
+        `[ERROR]: Cannot find ${chalk.bold(
+          "package.json",
+        )}. Please ensure that the file exists in the project root. \n`,
+      ),
+    );
+    return {};
+  }
+
   const packageJSON: {
-    dependencies: Record<string, string>;
-    devDependencies: Record<string, string>;
-  } = JSON.parse(
-    await fsPromises.readFile("package.json", { encoding: "utf8" }),
-  );
+    dependencies?: Record<string, string>;
+    devDependencies?: Record<string, string>;
+  } = JSON.parse(packageContent);
 
   return packageJSON;
 };
