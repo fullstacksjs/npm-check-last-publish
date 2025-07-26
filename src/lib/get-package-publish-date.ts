dc4077fca4531915d8f084904d24f1df2f99fea6
Amirreza <dropp.dev.amir.rahmani@gmail.com>
2025-07-27 03:12:17 +0330

    feat: allow checking specific packages via CLI args

--- src/lib/get-package-publish-date.ts
+++ src/lib/get-package-publish-date.ts
@@ -1,8 +1,15 @@
 import { getPackageVersionsDetail } from "./get-package-versions-detail.js";
 
 export const getPackagePublishDate = async (packageName: string) => {
-  const { packageVersion, publishedTimes } =
-    await getPackageVersionsDetail(packageName);
-  const packagePublishDate = new Date(publishedTimes[packageVersion]);
-  return { packagePublishDate, packageVersion, packageName, publishedTimes };
+  try {
+    const { packageVersion, publishedTimes } =
+      await getPackageVersionsDetail(packageName);
+    const packagePublishDate = new Date(publishedTimes[packageVersion]);
+    return { packagePublishDate, packageVersion, packageName, publishedTimes };
+  } catch (error: unknown) {
+    const e = error as Error;
+    throw new Error(
+      `Failed to fetch package info for "${packageName}": ${e.message}`,
+    );
+  }
 };
