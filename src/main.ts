4502fbed6283c2c8b22f2a89463da114a5975aa9
Mahdi Sheibak <mahdi.zoraghi.sheibak@gmail.com>
2025-10-20 14:36:24 +0330

    refactor: flag output

--- src/main.ts
+++ src/main.ts
@@ -8,60 +8,49 @@ import { processPackageData } from "./lib/process-packages.js";
 import { progressBar } from "./lib/progress-bar.js";
 import { renderTable } from "./lib/render-table.js";
 
-async function main() {
-  try {
-    const {
-      packages,
-      sortBy,
-      sortOrder,
-      pattern,
-      thresholds,
-      jsonExport,
-      csvExport,
-    } = getCliOptions();
+try {
+  const { packages, sortBy, sortOrder, pattern, thresholds, output } =
+    getCliOptions();
 
-    const { results, errors } = await fetchPackageInfoList(packages, pattern);
+  const { results, errors } = await fetchPackageInfoList(packages, pattern);
 
-    const sortedInfo = processPackageData({
-      results: results.filter((r) => r !== null),
-      sortBy,
-      sortOrder,
-      thresholds,
-    });
+  const sortedInfo = processPackageData({
+    results: results.filter((r) => r !== null),
+    sortBy,
+    sortOrder,
+    thresholds,
+  });
 
-    if (csvExport) {
-      const csvRows = [
-        Object.keys(sortedInfo[0]),
-        ...sortedInfo.map((info) => [
-          info.name,
-          info.version,
-          info.date,
-          info.diffDays,
-          info.area,
-          info.averagePublishDays,
-        ]),
-      ];
-      const csvContent = csvRows.map((row) => row.join(",")).join("\n");
-      console.log(csvContent);
-    } else if (jsonExport) {
-      console.log(JSON.stringify(sortedInfo, null, 2));
-    } else {
-      renderTable(sortedInfo);
-    }
-
-    if (errors.length > 0) {
-      for (const { package: pkg, error } of errors) {
-        console.warn(
-          styleText(["black", "bgYellow"], " WARNING "),
-          styleText("yellow", `${pkg}: ${error.message}\n`),
-        );
-      }
-    }
-  } catch (err) {
-    progressBar.stop();
-    console.error("Error:", err instanceof Error ? err.message : err);
-    process.exit(1);
+  if (output === "csv") {
+    const csvRows = [
+      Object.keys(sortedInfo[0]),
+      ...sortedInfo.map((info) => [
+        info.name,
+        info.version,
+        info.date,
+        info.diffDays,
+        info.area,
+        info.averagePublishDays,
+      ]),
+    ];
+    const csvContent = csvRows.map((row) => row.join(",")).join("\n");
+    console.log(csvContent);
+  } else if (output === "json") {
+    console.log(JSON.stringify(sortedInfo, null, 2));
+  } else {
+    renderTable(sortedInfo);
   }
-}
 
-main();
+  if (errors.length > 0) {
+    for (const { package: pkg, error } of errors) {
+      console.warn(
+        styleText(["black", "bgYellow"], " WARNING "),
+        styleText("yellow", `${pkg}: ${error.message}\n`),
+      );
+    }
+  }
+} catch (err) {
+  progressBar.stop();
+  console.error("Error:", err instanceof Error ? err.message : err);
+  process.exit(1);
+}
